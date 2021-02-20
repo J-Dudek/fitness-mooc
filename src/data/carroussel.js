c0e6c4085d15d1ecb77db6aedad3ce166f3166fa
julien dudek <>
2021-02-21 00:08:18 +0100

    🌱

--- src/data/carroussel.js
+++ src/data/carroussel.js
@@ -2,7 +2,7 @@ const items = [{ "id": "0", "title": "Change your life", "alt": "First slide", "
 
 { "id": "1", "title": "Learn about sport", "alt": "Second slide slide", "txt": "Here you will gain in sports skills", "url": "https://france3-regions.francetvinfo.fr/image/wTj3HhMNpHZ9Ym2B-TiCyj7pDFE/600x400/regions/2020/06/09/5edf9b1143796_renelle_lamote-4568263.jpg" },
 
-{ "id": "2", "title": "Be happy", "alt": "Third slide", "txt": "A healthy mind in a healthy body", "url": "https://lh3.googleusercontent.com/proxy/iWO35t0DGm3PNykugU6wXI-y7qq0Y53XNCzbMSGZoO9eFpAf7CamB9fzX7fImhujSjhaSdM-IaAm8XcaEr7Y0F9c8RsyWGmWHBc-PX1o4nhjN0lVibMXemq8hYWVoSgcdDOYMBxQuUIQchlf" }
+{ "id": "2", "title": "Be happy", "alt": "Third slide", "txt": "A healthy mind in a healthy body", "url": "https://www.univ-catholille.fr/sites/default/files/actus/1.JPG" }
 ];
 
 export default items;
