ffb5220dc85a41d897d817b10c93fc86e5250f34
julien dudek <>
2021-02-21 22:59:06 +0100

    🐛 build

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Intro from '../components/carroussel/caroussel'
+import Intro from '../components/carroussel/caroussel.js'
 import items from '../data/carroussel'
 import Article from '../components/article/article'
 const Home = () => (
@@ -10,7 +10,6 @@ const Home = () => (
         <Article title='What will you learn?' shorttext="The solid basics of sport" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit." />
         <Article title='Who are we?' shorttext="Students of M1 III" text=" Student of University Catholic of Lille in the first year of master degree." />
         <Article title='Why we do this MOOC?' shorttext="English course of Semester 2" text="In context of our course" />
-
     </div>
 );
 
