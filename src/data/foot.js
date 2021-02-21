7611082aac473f9fbac2eb1e5d23f9d30b9b6521
julien dudek <>
2021-02-21 23:21:23 +0100

    🚀

--- src/data/foot.js
+++ src/data/foot.js
@@ -1,13 +1,13 @@
 const foot = {
-    desc: "Project done during S2 of Master Class at the University Catholic of Lille",
-    autors: [{
-        id: "1",
-        identity: "Julien Dudek",
-        mail: "julien.dudek@lacatholille.fr"
+    "desc": "Project done during S2 of Master Class at the University Catholic of Lille",
+    "autors": [{
+        "id": "1",
+        "identity": "Julien Dudek",
+        "mail": "julien.dudek@lacatholille.fr"
     }, {
-        id: "2",
-        identity: "Another identity",
-        mail: "a mail"
+        "id": "2",
+        "identity": "Another identity",
+        "mail": "a mail"
     }
     ]
 }
