883eee89dccc5d4b5b3fbc86844f5164662af781
julien dudek <>
2021-02-21 22:44:56 +0100

    🚧 upgrade footer with autors

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,8 @@ import React from "react";
 import './footer.css';
 const Footer = ({ foot }) => (
     <div className="footer">
-        <p>{foot}</p>
+        <p>{foot.desc}</p>
+        {foot.autors.map(aut => <span key={aut.id}> {aut.identity} : {aut.mail}</span>)}
     </div>
 );
 
--- src/data/foot.js
+++ src/data/foot.js
@@ -1,3 +1,15 @@
-const foot = `Project done during S2 of Master Class at the University Catholic of Lille`;
+const foot = {
+    desc: "Project done during S2 of Master Class at the University Catholic of Lille",
+    autors: [{
+        id: "1",
+        identity: "Julien Dudek",
+        mail: "julien.dudek@lacatholille.fr"
+    }, {
+        id: "2",
+        identity: "Another identity",
+        mail: "a mail"
+    }
+    ]
+}
 
 export default foot;
