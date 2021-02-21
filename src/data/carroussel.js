13c4d7eebfb677af357955b0fd498499baba1ea4
julien dudek <>
2021-02-21 01:02:01 +0100

    🎨

--- src/data/carroussel.js
+++ src/data/carroussel.js
@@ -1,8 +1,30 @@
-const items = [{ "id": "0", "title": "Change your life", "alt": "First slide", "txt": "With this program you can change your vision of the world", "url": "https://qiis-wordpress-eticket.s3.amazonaws.com/wp-content/uploads/2019/02/13152131/article-sport.jpg" },
+import Img1 from './img/first.jpg'
+import Img2 from './img/second.jpeg'
+import Img3 from './img/third.jpeg'
 
-{ "id": "1", "title": "Learn about sport", "alt": "Second slide slide", "txt": "Here you will gain in sports skills", "url": "https://france3-regions.francetvinfo.fr/image/wTj3HhMNpHZ9Ym2B-TiCyj7pDFE/600x400/regions/2020/06/09/5edf9b1143796_renelle_lamote-4568263.jpg" },
+const items = [{
+    "id": "0",
+    "title": "Change your life",
+    "alt": "First slide",
+    "txt": "With this program you can change your vision of the world",
+    "url": Img1
+},
 
-{ "id": "2", "title": "Be happy", "alt": "Third slide", "txt": "A healthy mind in a healthy body", "url": "https://www.univ-catholille.fr/sites/default/files/actus/1.JPG" }
+{
+    "id": "1",
+    "title": "Learn about sport",
+    "alt": "Second slide slide",
+    "txt": "Here you will gain in sports skills",
+    "url": Img2
+},
+
+{
+    "id": "2",
+    "title": "Be happy",
+    "alt": "Third slide",
+    "txt": "A healthy mind in a healthy body",
+    "url": Img3,
+}
 ];
 
 export default items;
