09f21f279cadc997d4306cc7c5d0c3919ae08df7
julien dudek <>
2021-02-23 14:48:42 +0100

    📱 Footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import './footer.css';
 const Footer = ({ foot }) => (
     <div className="footer">
         <p>{foot.desc}</p>
-        {foot.autors.map(aut => <span key={aut.id}> {aut.identity} : {aut.mail}</span>)}
+        {foot.autors.map(aut => <div key={aut.id}> {aut.identity} : {aut.mail}</div>)}
     </div>
 );
 
