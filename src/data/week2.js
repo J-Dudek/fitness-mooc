ec51739d7a14ce8475ceb0629eba0caa8577ca54
julien dudek <>
2021-02-24 17:31:13 +0100

    ✨ Run

--- src/data/week2.js
+++ src/data/week2.js
@@ -3,18 +3,105 @@ const cards = {
     "content": [
         {
             "id": "0",
-            "title": "Top of body",
-            "program": "This week we'll talk about..."
+            "title": "Focus on Running",
+            "program": `<h3>Why run?</h3>Several studies show that running is a great way to lose weight, quit smoking, or prevent various cardiovascular diseases. But getting involved in running, while it is a natural movement, is not that simple. Indeed, many will become disgusted and/or hurt themselves.
+            this is why this week we are going to give you some tips to avoid these inconveniences and for those who are already running we will give you some avenues for improvement.`
         },
         {
             "id": "1",
-            "title": "Somes exercices",
-            "program": "Blabla ...."
+            "title": "A bit of history and records",
+            "program": `<h3>Since -490</h3>
+            One of the founding dates in the history of running dates back to -490 in ancient Greece. The Athenians are then at war against the Persians near the city of Marathon. The story goes that a messenger named Phillipides was sent from Marathon to warn Athens of the victory of the Greeks. The two towns are then separated by 42 km. The messenger would have died of fatigue just after delivering his message.
+            <div>The marathon was created on the occasion of the Athens Olympics in 1896, on an idea of the French philosopher Michel Bréal, to commemorate the legend of this messenger.</div>
+            <h3>Since 1921</h3>
+            The marathon distance was around 40 km in the early modern Olympic Games. It was in 1921 that the distance was definitively fixed, by formalizing that of the London Olympic Games (1908). The royal family of Edward VII indeed wishing that the race begins at Windsor Castle and ends in front of the royal box of the Olympic stadium.
+            This distance was therefore measured precisely: 26 miles and 385 yards or 42.195 km and became the official distance of the marathon.
+            <h3>Actually</h3>
+            The world records for the marathon are currently held by Kenyan Eliud Kipchoge with the time of 2 h 1 min 39 s set on September 16, 2018 during the marathon in Berlin, Germany1, and by the Kenyan Brigid Kosgei, credited with 2:14 min 4 s on October 13, 2019 during the Chicago Marathon in USA 2. The IAAF also awards a women's world record to Kenya's Mary Keitany in 2:17:01, a performance set on April 23, 2017 at London in a women's all-race.
+            `
         },
         {
             "id": "2",
-            "title": "And..",
-            "program": "No video this week"
+            "title": "Slowly but surely: endurance",
+            "program": `One of the essential concepts to acquire is that of <strong>fundamental endurance</strong>.
+            Fundamental endurance is a running pace that you can theoretically run without stopping.
+            Indeed, you are not out of breath at this rate; you are comfortable. Usually, this is the outing you do while chatting with your friends
+            This pace should be the basis of your training. It should represent 70% of your training volume because it helps develop many of your physical capacities.
+            </br>
+            By running a long time in fundamental endurance, you <strong>muscle your heart</strong> and you increase its volume. More powerful, the latter sends more blood into your body. Your heart rate (HR) then drops for the same effort.
+            </br>
+            By running regularly with this technique, you <strong>develop your capillarization</strong>. This is because your body will improve the network of capillaries that supply oxygen to your muscles. They will thus be able to benefit from more oxygen and be more efficient.
+            </br>
+            Endurance training <strong>increases the mitochondria in muscle fibers</strong>. These mitochondria, located in muscle cells, convert “glucose” into ATP, the energy that can be directly used by muscle cells. We quickly understand the importance of increasing this number of mitochondria in a context of progression and performance.
+            </br>
+            An ideal pace for soliciting fat and activating the lipid chain
+            During long outings in fundamental endurance, and in particular on an empty stomach, the runner will make better use of lipid reserves. The interest can be multiple:
+            <ul>
+                <li>Lose weight by melting fat.</li>
+                <li>Running for a long time:</li> whether on a trail, an ultra-trail, a 24 hour race or even a 100 kiosk, the goal is to last a long time. However, carbohydrate reserves are limited, unlike lipid reserves. To last over a long race, it will therefore be necessary to be able to use the lipid sector as effectively as possible.
+                <li>Pushing back the wall of the marathon:</li> the wall of the marathon corresponds to the depletion of carbohydrate reserves and therefore the lack of energy. Training in fundamental endurance makes it possible to learn to use lipids as an energy chain and no longer rely solely on the carbohydrate chain.
+            </ul>
+            
+            Running in fundamental endurance is <strong>much less traumatic</strong> for the body than so-called "quality" (split) sessions. This is why you can hold this pace for a very long time.
+            This pace will allow you to increase your weekly volume, whether running or trail running. Be careful, however, this increase in volume should be gradual to avoid injuring yourself (tendonitis, windshield wiper syndrome, etc.).
+            As you tire your body less, you can run more often. So, to run every day, you will have to run no faster than this pace.`
+        },
+        {
+            "id": "3",
+            "title": "First exercice",
+            "program": `As you have understood, the first exercise will be to do a short race on the theme of fundamental endurance.
+            For this race, if you are lucky enough to know a runner and he wants to accompany you: go for it! But not too fast!
+            You can also be accompanied by a loved one by bike or other .. the idea is to share this moment.
+            I remind you: you must be able to continue discussing with your partner.
+            </br>
+            <h3>How long?</h3>
+            For beginners, half an hour seems fine. With an effort of this intensity and duration, you shouldn't have too much muscle pain.
+            For experienced runners, it doesn't matter how long your body is used to this kind of effort and your muscles are ready.`
+        },
+        {
+            "id": "4",
+            "title": "After the session",
+            "program": `<h3>In the shower</h3>
+            You deserved a good shower. A little advice, during this shower you will apply temperature changes on your legs from your usual temperature to the coldest temperature available. Do it gradually if you are not used to it, but do it. The first time it may seem quite difficult but you will get used to it and it will allow you not to have aches the next day.
+            <h3>Hydrate yourself</h3>
+            The legend says that it is good to share a good beer with friends after a session of running. Truth or just excuse? .. What is certain is that after having subjected your body to physical efforts, you must rehydrate it. In general, in sports it is advisable to drink plenty of water. You will also find in many specialized stores as well as in pharmacies drinks and recovery products. By having tested a few I can advise you those based on spirulina, I take it in exchange for an intensive outing.
+            <h3>Stretching</h3>
+            For this part I send you to the week which deals with stretching. In the case of running, there are several schools, some do stretching before, during or after their training. I do them totally outside of activity and sometimes just when I have time. I am not necessarily a reference on the subject, but each body being different, each person does not have the same needs.
+            `
+        },
+        {
+            "id": "5",
+            "title": "Interval training",
+            "program": `<h3>Why ?</h3>
+            This type of training will allow you to improve your speed and also your endurance.
+            You will find tons of types of split sessions on the internet and in magazines.
+            In all these articles you will come across technical terms such as VMA (Maximum Aerobic Speed), FCR (Heart Rate Reserve).
+            In our case we will talk about VMA.
+            </br>
+            <h4>The 30/30 to develop the VMA</h4>
+            Found in a large number of training plans, the principle is extremely simple: 30 seconds fast and 30 seconds slow, to be repeated several times.
+            <ul>
+                <li>Beginner:</li>
+                    10 x 30 sec (at 100% VMA / 30sec light trot)
+                <li>Expert:</li>
+                    2 to 3 sets of 10 x 30sec (at 105% VMA / 30 sec light trot) 3min recovery between sets
+            </ul>
+            <h4>The session at the threshold</h4>
+            Indispensable for building up the fund, understanding the ability to last in the effort.
+            </br>
+            Warm-up + 3 x 10 min at the threshold (Recovery 3 min trot) + 10 min calm down.
+            <h4>Playful : short pyramid</h4>
+            This session allows you to break up the monotony of the training sessions and to work on a whole range of gaits close to the VMA.
+            The rising part of the pyramid is mentally demanding. But when you reach the top the downward part rekindles your motivation and learns not to let go.
+            <ul>
+                <li>Typical session :</li>
+                    100/200/300/400/500/400/300/200/100
+                <li>Recovery :</li>
+                    30sec/40sec/50sec/1min/1min10/1min/50sec/40sec
+                <li>Swept intensity :</li>
+                    de 105% VMA sur les 100m à 95% sur les 500m
+            </ul>
+            `
         }
     ]
 };
