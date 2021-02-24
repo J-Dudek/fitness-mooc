a6545f65b2408f91ca01fed1f233889890a55a7d
julien dudek <>
2021-02-24 20:56:19 +0100

    🔥 remove import and other

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,27 +1,26 @@
 import React from "react";
 import './article.css'
 import Identity from '../identity/identity';
-import { CardDeck } from 'react-bootstrap'
 const Article = ({ article }) => (
     <div className="section">
 
-        { (article.position && article.position === 'left') ?  <div className="left img"/> : ''}
-        <div id={article.position && article.position === 'center'? 'full' : ''} className={`text flex-center`}>
+        { (article.position && article.position === 'left') ? <div className="left img" /> : ''}
+        <div id={article.position && article.position === 'center' ? 'full' : ''} className={`text flex-center`}>
             <div className="text-container flex-center">
                 <h2>{article.title}</h2>
                 <p>{article.shorttext}</p>
                 <p>{article.text}</p>
-                { (article.team  ) ?
+                {(article.team) ?
                     <div className={'container flex-grid'}>
                         {article.team.map(member =>
-                        <Identity member={member}/> )}
+                            <Identity member={member} />)}
                     </div>
                     : <a href={'/first'} className="btn">Commencer</a>
                 }
             </div>
         </div>
         { /* Pour afficher l'image à droite */
-            (article.position && article.position === 'right') ?  <div className="right img"/> : ''
+            (article.position && article.position === 'right') ? <div className="right img" /> : ''
         }
     </div>
 
--- src/components/identity/identity.js
+++ src/components/identity/identity.js
@@ -1,10 +1,10 @@
-import { Card } from 'react-bootstrap'
+
 import './identity.css'
 const Identity = ({ member }) => (
     <div className="person">
-        <img src={member.avatar} alt={`avatar de ${member.firstname}`}/>
-            <p className="name">{member.firstname}</p>
-            <p>{member.bio}</p>
+        <img src={member.avatar} alt={`avatar de ${member.firstname}`} />
+        <p className="name">{member.firstname}</p>
+        <p>{member.bio}</p>
     </div>
 );
 
