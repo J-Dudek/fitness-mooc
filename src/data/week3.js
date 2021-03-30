02b9a68bfb9461675e2ee9ff0630aa1854d47fa1
Julien Dudek <julien.dudek@gmail.com>
2021-03-30 21:57:22 +0200

    ✨  week3 WIP

--- src/data/week3.js
+++ src/data/week3.js
@@ -1,5 +1,8 @@
 import muscleGroups from './img/week3/muscleGroups.jpg'
-import abdominals from './img/week3/absWorkout.jpg'
+import basicPlank from './img/week3/basicPlank.png'
+import sidePlank from './img/week3/sidePlank.png'
+import mountainClimber from './img/week3/mountainClimber.jpg'
+import anatomyAbdo from './img/week3/abdominals.jpeg'
 const cards = {
     "title": "Third Week - GPP",
     "autor": "Julien Dudek",
@@ -51,16 +54,77 @@ const cards = {
             ]
         }, {
             "id": "3",
-            "title": "UPPER BODY",
+            "title": "Abdominals",
             "program": [{
                 id: 1,
-                subtitle: "Abdominals",
-                content: `Speech about abdox ;)`,
-                img: abdominals
+                subtitle: "Anatomy",
+                content: `Too often neglected in physical preparation, the abdominals are nevertheless among the most essential muscles for work, whatever the discipline practiced.
+                The abdominals being postural muscles, they are used, no matter what, on a daily basis. But that is not enough to develop them. The ideal training frequency: every other day or 3 to 4 times a week, to give them time to recover properly between each session.
+                The abdominal muscles are divided into two parts: the superficial abdominals and the deep abdominals. The superficial abdominals are made up of the rectus abdominis (which runs from the lower part of the pectorals to the pubis) and obliques (located on the sides, they are separate from the rectus abdominis but are part of the abdominal strap training).
+                The deep abdominals correspond to the transverse. The latter is deep, it surrounds the entire abdomen and brings the navel closer to the spine when exhaling. It is this muscle that helps keep your stomach flat.`,
+                img: anatomyAbdo
             },
             {
                 id: 2,
-                content: `We therefore strongly advise you to vary the pleasures and to work one day the upper body and the other the lower one. You can do a full and particularly intense session at the end of the week, or on the day you want, but the next day it is mandatory to rest.`
+                subtitle: "RECTUS ABDOMINIS : CRUNCH",
+                content: `It particularly works the rectus abdominis, the muscle that runs up and down the anterior wall of the abdomen. This is actually the muscle you need to work on to have the famous "chocolate bars" so adored by athletes and bodybuilders. It is the spinal curl bringing the bust closer to the pubis and thighs that is responsible for this result. The area above the navel is therefore the most reinforced. You can also work the deep abdominal or transverse muscle by doing a crunch. To achieve your goal, you must then contract your stomach well during the exercise.
+                It should then be understood that crunch abs are aesthetic exercises that allow the abdominals to take shape. Yet, they are often wrongly practiced to lose belly fat. To reduce the abdominal perimeter, it is better to start by increasing the basal metabolism before moving on to muscle building.
+                The video below will give you the best way to crunch.`,
+                url: `https://youtu.be/LrRm-Cg2wQU`,
+
+            }, {
+                id: 3,
+                subtitle: "DEEP MUSCLES,LUMBAR and rectus abdominis : PLANK EXERCISE",
+                content: `It works the rectus abdominis but also the obliques and the transverse, a deep muscle that works well to achieve a flat stomach. Supporting the arms tones the biceps, triceps and deltoids. Finally, contraction and maintaining balance keep your back, glutes, hamstrings and quads working.
+                This abdominal sheathing movement will not allow you to gain muscle mass or volume in the abs. The goal is more "internal" with the aim of strengthening the deep muscles. In addition, the board contributes to a better transmission of forces between the upper and lower body, as well as to work on balance, more or less extensive depending on the variants performed.`,
+                diapo: [
+                    {
+                        "id": "0",
+                        "title": "Basic Plank",
+                        "alt": "First slide",
+                        "txt": "",
+                        "url": basicPlank
+                    },
+
+                    {
+                        "id": "1",
+                        "title": "Side Plank",
+                        "alt": "Second slide slide",
+                        "txt": "",
+                        "url": sidePlank
+                    },
+                ]
+            }, {
+                id: 4,
+                subtitle: 'Abdominals but not only : Mountain Climber',
+                content: 'The last exercise is the mountain climber. This exercise works the abdominals and also the legs, shoulders and elasticity of the muscles of the pelvis. It is a great calorie burn exercise. As you can see after a series of twenty repetitions. This exercise also works the cardio.The gesture is simple and very effective, it consists of lifting one knee towards the torso by contracting the abdominal belt. A variation is to orient the knee towards the opposite shoulder. This version is more effective but I advise you to start with the basic version during your first sessions then to vary the pleasures to avoid getting bored',
+                img: mountainClimber,
+            }
+            ]
+        }, {
+            "id": "4",
+            "title": "Lower Body",
+            "program": [{
+                id: 1,
+                subtitle: "Which muscles are we going to work?",
+                content: `In this part, we will do several exercises to work the lower muscles such as the hamstrings, glutes, and spinal erectors. We will then see how to strengthen your calves.`,
+            },
+            {
+                id: 2,
+                subtitle: "About exercices",
+                content: `As often, it is strongly recommended to alternate the exercises in order not to create muscle imbalance. I therefore advise you to chain each exercise seen below so that this sequence creates a circuit that you can do between three and five times. One minute of recovery between each series will be sufficient.`
+            }, {
+                id: 3,
+                subtitle: 'Squats',
+                content: `The main muscles that work during a squat are the quads and glutes. This will lead to bigger thighs and more plump buttocks.
+                You will understand, squats allow you to increase your muscle mass and therefore you will get a more efficient metabolism. A more efficient metabolism will cause your body to consume more energy while resting. yes, squat, like almost all sports, will burn fat.
+                In addition to these benefits, squats will help you maintain better posture and therefore have fewer back problems.Back pain being recognized as a disease of the century if there is one exercise to be done regularly, it is this one.`,
+                url: `https://www.youtube.com/watch?v=x9nencVW93M`,
+            }, {
+                id: 4,
+                subtitle: 'Lunges',
+                content: `Lunges are the perfect exercise for working your glutes and shaping your legs. They work the entire thigh and back of the leg. Lunges are some of the most dynamic exercises around and are sure to make you sweat. For example, a Lunges workout lasting 15 to 20 minutes will burn approximately 100 calories. However, this is only a guide. There are the so-called classic lunges and the side lunges. As all exercices lunges can be done without material as well as while carrying a weight. Here you can see a step by step for doing this exercice .`,
+
             }
             ]
         },
