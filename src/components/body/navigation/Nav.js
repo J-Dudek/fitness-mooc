848c18be4ee7d7e7705bc9aa27a7c20afd8137fd
Thais Revillon <thais.r@live.fr>
2021-04-07 22:05:45 +0200

    :art: nav's dropdown on phone ok

--- src/components/body/navigation/Nav.js
+++ src/components/body/navigation/Nav.js
@@ -2,6 +2,20 @@ import React from 'react';
 import './nav.css';
 
 class CustomNav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+        this.setState({link_class: 'hidden'});
+        this.openNav = this.openNav.bind(this);
+
+    }
+
+    openNav() {
+        this.myRef.current.style.display =
+            (this.myRef.current.style.display === 'flex') ?
+            'none': 'flex';
+    }
+
     render() {
         return (
             <nav id="nav-container" className="flex-grid">
@@ -9,11 +23,11 @@ class CustomNav extends React.Component {
                     <h1><a id="title" href={'/'}>Fitness Mooc</a></h1>
                 </div>
                 <div id="nav-dropdown" className="flex-center">
-                    <button id="nav-btn">
+                    <button id="nav-btn" onClick={this.openNav}>
                         <span id="btn-icon" className="material-icons">menu</span>
                     </button>
                 </div>
-                <div id="links-container">
+                <div id="links-container" ref={this.myRef}>
                     <a href={'/first'} className="link">First Week</a>
                     <a href={'/second'} className="link">Second Week</a>
                     <a href={'/third'} className="link">Third Week</a>
